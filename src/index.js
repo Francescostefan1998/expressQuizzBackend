99b48efc0a7776498dcbb41964d51ce36cc78394
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-10-12 10:14:26 +0200

    adding cors

--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ dotenv.config();
 
 // 2. Initialize Express app
 const app = express();
-app.use(
-  cors({
-    origin: true,
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Consentire tutte le origini (assicurati di limitarlo per la produzione)
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;
 
