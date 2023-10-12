3fa6f306e0d04a190c8d42499d2d0019ce59c56f
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-10-12 11:47:22 +0200

    d

--- src/index.js
+++ src/index.js
@@ -157,8 +157,7 @@ app.get(
         },
         token,
       });
-      const frontendURL =
-        "https://frontend-would-you-rather-7tt1h91to.vercel.app";
+      const frontendURL = "https://frontend-would-you-rather.vercel.app/";
       res.redirect(`${frontendURL}/google-auth-redirect?data=${response}`);
     } else {
       res.status(500).send("Authentication failed.");
