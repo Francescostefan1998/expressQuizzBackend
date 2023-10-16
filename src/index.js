0aca9055506a1d42a0181ee4a418a3a63a093cb4
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-10-16 12:17:50 +0200

    try to use redis

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import session from "express-session";
 import passport from "./passport-setup.js";
 import googleRouter from "./googleLoginEndpoint.js";
 import userRouter from "./users.js";
+// index.js
+
+import session from "express-session";
+const RedisStore = require("connect-redis")(session);
+const redis = require("redis").createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
 dotenv.config();
 
 const app = express();
@@ -22,9 +30,11 @@ app.use(
 );
 
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;
+const sessionStore = new RedisStore({ client: redis });
 
 app.use(
   session({
+    store: sessionStore,
     secret: process.env.SESSION_SECRET || "some_random_secret_key",
     resave: false,
     saveUninitialized: true,
